048cb53494c0a9806036d8bd1c05dbdd7b7d0e16
Cucuzacu <150786439+Cucuzacu@users.noreply.github.com>
2024-09-20 17:40:48 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+//ascii animation code in crap
+//made by cuuczacu
+
+#include<fstream>
+#include<iostream>
+
+int main()
+{
+    int n, k, i, j;
+    static int c[101][101];
+    std::ifstream fi("frames.txt");
+    std::ofstream fo("output.txt");
+    std::cin >> n;
+    for(k = 0; k < n; k++)
+    {
+        for(i = 0; i < 100; i++) std::cin >> c[i];
+        for(i = 0; i < 100; i++)
+            for(j = 0; j < 100; j++)
+                fo << "printstring " << char(c[i][j]) << '\n';
+        fo << "clear" << '\n';
+        fo << "wait" << '\n';
+    }
+}
