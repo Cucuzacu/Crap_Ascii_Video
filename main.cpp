e9e61788dea483c09950826c00b8b17fa9b9fdef
Cucuzacu <150786439+Cucuzacu@users.noreply.github.com>
2025-05-18 22:41:19 +0300

    Final Version

--- main.cpp
+++ main.cpp
@@ -1,23 +1,31 @@
 //ascii animation code in crap
 //made by cucuzacu
+//reads number of frames and time to wait from keyboard
 
 #include<fstream>
 #include<iostream>
 
+#define max_lenght_of_line 100
+#define lines_of_frame 36
+
 int main()
 {
-    int n, k, i, j;
-    static int c[101][101];
+    int n, w, k, i, j;
+    char c[max_lenght_of_line + 100];
     std::ifstream fi("frames.txt");
-    std::ofstream fo("output.txt");
-    std::cin >> n;
-    for(k = 0; k < n; k++)
+    std::ofstream fo("code.crap");
+    std::cin >> n >> w;
+    for (k = 0; k < n; k++)
     {
-        for(i = 0; i < 100; i++) std::cin >> c[i];
-        for(i = 0; i < 100; i++)
-            for(j = 0; j < 100; j++)
-                fo << "printstring " << char(c[i][j]) << '\n';
+        for (i = 0; i < lines_of_frame; i++)
+        {
+            fi >> c;
+            fo << "printstring " << c << '\n';
+            fo << "nextline" << '\n';
+        }
         fo << "clear" << '\n';
-        fo << "wait" << '\n';
+        fo << "wait " << w << '\n';
+        system("cls");
+        std::cout << k + 1 << " frame(s) processed!";
     }
 }
